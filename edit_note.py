91f5889f462a1b4a0f116c4c17ae76c6a1943bb1
Borriiiissss <chega123@mail.ru>
2023-02-13 20:05:42 +0200

    final

---
+++ edit_note.py
@@ -0,0 +1,17 @@
+import json
+from save_note import save_note
+
+def edit_note():
+    print ('input id of note for edit ->')
+    user_input = input ()
+    with open("data.json", "r") as file:       
+        while True:
+            line = file.readline()
+            if not line:
+                print("нет такого id")
+                break
+            data = json.loads(line)
+            if (data['id'] == int(user_input)):
+                print(data['note_title'])
+                print(data['note'])
+                save_note(data['id'])
--- save_note.py
+++ save_note.py
@@ -1,12 +1,53 @@
 import json
+import datetime
 
-def save_note ():
-    note_title = (input ('введите заголовок заметки'))
-    note = (input ('введите заметку'))
-    data = {note_title: note}
-
-    with open('data.txt', 'a') as outfile:
-        outfile.write("\n")
-        json.dump(data, outfile, separators=("", ":"))
-        
+def save_note (*num_id):
+    with open("data.json", "r") as file:
+        edit = False
+        # определяем ID
+        if (not num_id):
+            if (sum(1 for line in open('data.json')) > 0):               
+                data = json.loads(file.readlines() [-1])             
+                num_id = data['id'] + 1
+                print (num_id) 
+            else:
+                num_id = 1
+        else:           
+            edit = True
+        note_title = (input ('введите заголовок заметки - '))
+        note = (input ('введите текст заметки - '))
+        mark = str(datetime.date.today())
+        note_data = {}
+        note_data = {'id': num_id, 'note_title' : note_title, 'note' : note, 'timemark' : mark}          
+        if (edit == False):
+            with open('data.json', 'a') as outfile:               
+                json.dump(note_data, outfile, ensure_ascii=False)
+                outfile.writelines('\n')
+                print ("строка добавлена ")        
+        else:           
+            open('tmpdata.json', 'w').close()
+            with open('data.json', 'r') as file:
+                while True:
+                    line = file.readline()
+                    if not line:
+                        break
+                    data = json.loads(line)  
+                    if data['id'] != num_id:                                   
+                        with open('tmpdata.json', 'a') as tmpfile:
+                            json.dump(data, tmpfile, ensure_ascii=False)
+                            tmpfile.writelines('\n')
+                    else:
+                        with open('tmpdata.json', 'a') as tmpfile:
+                            json.dump(note_data, tmpfile, ensure_ascii=False)
+                            tmpfile.writelines('\n')
+            open('data.json', 'w').close()
+            with open('tmpdata.json', 'r') as file:
+                while True:
+                    line = file.readline()
+                    if not line:
+                        break
+                    data = json.loads(line)                                   
+                    with open('data.json', 'a') as newfile:
+                        json.dump(data, newfile, ensure_ascii=False)
+                        newfile.writelines('\n')          
     return
